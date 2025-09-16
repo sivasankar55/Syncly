1b90abeaa7f966262032cfb780a01c2386be2c7b
sivasankar55 <sivasankar8875@gmail.com>
2025-09-16 18:05:18 +0530

    stream & sentry integration

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,12 @@
+import { generateStreamToken } from "../config/stream.js"
+
+export const getStreamToken = (req,res) => {
+    try {
+        const token = generateStreamToken(req.auth().userId);
+
+        res.status(200).json({token});
+    } catch (error) {
+        console.error("Error generating stream token:", error);
+        res.status(500).json({ error: "Failed to generate stream token" });
+    }
+};
